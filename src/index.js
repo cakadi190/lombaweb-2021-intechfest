dd3d52d89e9e370c44aa1a8676157566ada315fa
dasakreativa <dasakreativa@gmail.com>
2021-09-16 19:58:05 +0700

    Versi final

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Remove the following lines, if you do not need any of Bootstrap's JavaScript features
+import Popper from "popper.js";
+window.jQuery = $;
+window.$ = $;
+
+require("bootstrap");
+
+require("./page/home");
